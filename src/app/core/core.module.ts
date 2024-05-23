447d9512eb44b6b52151899893cb09682e8a8f99
Ramzi K.M <ramzikm3@gmail.com>
2024-05-23 17:18:34 +0530

    :lipstick: implement header footer ui

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { AppRoutingModule } from '../app-routing.module';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [HeaderComponent, FooterComponent],
+  imports: [CommonModule, AppRoutingModule],
+  exports: [HeaderComponent, FooterComponent],
 })
-export class CoreModule { }
+export class CoreModule {}
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  darkMode: "selector",
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      cXl: "1200px",
+      xl: "1440px",
+    },
+  },
+  daisyui: {
+    themes: ["retro", "dim"],
+  },
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
+};
